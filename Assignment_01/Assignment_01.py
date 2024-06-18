fde54c5d8107091ea99bdbc4966a05e47638d5be
Abdul Sattar <115935420+asbrohi@users.noreply.github.com>
2024-06-19 01:10:34 +0500

    Add files via upload

---
+++ Assignment_01/Assignment_01.py
@@ -0,0 +1,9 @@
+#This is Assignment 01 from Bytweise 100Days Fellowship
+name = "Abdul"
+age = 23
+#Using formatted string
+print(f"I am {name} and I am {age} years old!")
+
+# Looking for the id and type of variables
+print(type(name))
+print(id(age))
